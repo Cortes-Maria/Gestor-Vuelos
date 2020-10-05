c72aa3d89e1f67d4b881f6d85cb3a4b0b81a7414
María José Cortés <maria2000jose.mj@gmail.com>
2020-10-04 21:24:41 -0600

    Estadisticas
    Segunda funcionalidad de estadistica implementada

--- estadisticas.h
+++ estadisticas.h
@@ -2,7 +2,10 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <mysql.h>
+#include "conecta_mySQL.h"
 #define TOP_VENTAS "CALL TOP3_MayorVenta()"
+#define TOP_CANT "CALL TOP3_MayorCantidad()"
+
 
 
 
@@ -23,7 +26,7 @@ void MayorVentas() {
 
     res = mysql_use_result(conn);
 
-    printf("TOP 3 DE VUELOS DE MAYOT VENTA!\n");
+    printf("TOP 3 DE VUELOS DE MAYOR VENTA!\n");
     	
 	while ((row = mysql_fetch_row(res)) != NULL) {
 		printf("%s","Monto de venta: ");
@@ -38,11 +41,42 @@ void MayorVentas() {
     mysql_close(conn);
 
 }
+void MayorCantidadPersonas() {
+    MYSQL *conn = mysql_init(NULL);
+    conn = conexion_mySQL();
+    MYSQL_RES *res;
+	MYSQL_ROW row;
+
+    int res_int;//Guarda el resultado de la query
+    char q_mostrarTOP3[300];//Guarda el query para iniciar sesión
+    strcpy(q_mostrarTOP3, TOP_CANT);//Agrega una parte de la query para la función
+
+    if (mysql_query(conn, q_mostrarTOP3)) {//Ejecuta la función de la base de datos
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        exit(1);
+    }
+
+    res = mysql_use_result(conn);
+
+    printf("TOP 3 DE VUELOS DE MAYO CANTIDAD DE PERSONAS!\n");
+    	
+	while ((row = mysql_fetch_row(res)) != NULL) {
+		printf("%s","Cantidad de personas: ");
+		printf("%s", row[0]);
+        printf("  ");
+        printf("%s", "Código de vuelo: ");
+        printf("%s", row[1]);
+        printf("\n");
+    }
+
+    mysql_free_result(res);
+    mysql_close(conn);
+}
 
 void menuEstadisticas() {
     int eleccion;
     do{
-        printf("\n  Registro de Aviones\n");
+        printf("\n      Menú estadísticas\n");
         printf( " __________________________________________________________________________________________\n");
         printf( "|                                                                                          |\n");
         printf( "| 1.Top 3 de vuelos de mayor venta (de mayor a menor)                                      |\n");
@@ -58,7 +92,7 @@ void menuEstadisticas() {
                 MayorVentas();
                 break;
             case 2:
-                
+                MayorCantidadPersonas();
                 break;
             case 3:
                 
